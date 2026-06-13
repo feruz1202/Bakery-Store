d2851ac6a73f0fc1f6454ee4598557d9ce9db420
Ikromov Feruz <ikromovferuz1202@gmail.com>
2026-06-13 13:34:19 +0500

    server.js

--- bakery-backend/server.js
+++ bakery-backend/server.js
@@ -7,7 +7,7 @@ const app = express()
 
 // Middleware
 app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:5174", "bakery-backend.railway.internal"],
+  origin: ["http://localhost:5173", "http://localhost:5174"],
   credentials: true
 }))
 app.use(express.json())
